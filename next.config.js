c10c584e0a8f363965a12611395784bf88e15ba8
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2022-05-24 18:47:06 -0400

    -

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+    SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN
+  },
   images: {
     domains: ["cdn.shopify.com"]
   }
--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,6 @@ export const getServerSideProps = async (context) => {
 
     return {
         props: {
-
         },
     }
 }
